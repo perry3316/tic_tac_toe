81dc6245f2b09c991f3cac843a305ad532905c27
Harshit Singh <h8harshitsingh@gmail.com>
2023-10-07 20:02:12 +0530

    improvements and changes:

--- header.h
+++
@@ -1,4 +0,0 @@
-#include "ANSI.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
--- main.c
+++ main.c
@@ -1,27 +1,40 @@
-#include "header.h"
-int start();      //print game menu on screen
-void game();      //call function for game
-void g_name();    //print tictactoe on screen
-void p_name();    //take player name as input
-void design();    //print pattren of game
-void input_pos(); //take position as input
-int chk_pos();    //check pattern for result
-void result();    //check result
-void final(int);  //print result
-void h_scr();     //hold screen
-void g_result();  //check result
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define ANSI escape sequences for text formatting
+#include "ANSI.h"
+
+// Global variables
 int l = 1, i_pos = 0, count = 0;
 int g_play = 0, p1_win = 0, p2_win = 0, g_draw = 0;
 int pl = 0, m = 0, b[99] = {}, h = 0;
 char p1[20], p2[20];
 char a[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char win[99][20], p1_n[99][20], p2_n[99][20];
+
+// Function prototypes
+int start();          // Print the game menu on the screen and get user choice
+void game();          // Start the game
+void g_name();        // Print "TicTacToe" on the screen
+void p_name();        // Take player names as input
+void design();        // Print the game board pattern
+void input_pos();     // Take player position as input
+int chk_pos();        // Check the game board for a result
+void result();        // Check and display the game result
+void final(int);      // Display the final result of the game
+void h_scr();         // Hold the screen for user interaction
+void g_result();      // Display game statistics and results
+
 int main()
 {
     while (1)
     {
+        // Clear the screen and set text formatting
         printf(CRH);
         system("clear||cls");
+
+        // Show the game menu and get user choice
         switch (start())
         {
         case 1:
@@ -39,24 +52,28 @@ int main()
             printf(B RRED "\n\tInvalid choice" RES);
             h_scr();
         }
+
+        // Reset game variables for a new game
         count = 0;
         l = 1;
         i_pos = 0;
         for (int i = 0; i < 9; i++)
-            a[i] = i + 49;
+            a[i] = i + 49; // Reset game board
     }
 }
+
 int start()
 {
     int ch;
-    printf(B RGRN "\n\t1.Play TicTacToe");
-    printf(RBLU "\n\t2.Game Result");
-    printf(RRED "\n\t3.Exit");
+    printf(B RGRN "\n\t1. Play TicTacToe");
+    printf(RBLU "\n\t2. Game Result");
+    printf(RRED "\n\t3. Exit");
     printf(RCYN "\n\tEnter your choice: ");
     scanf("%d", &ch);
     printf(RES);
     return (ch);
 }
+
 void game()
 {
     char m;
@@ -64,7 +81,7 @@ void game()
     g_name();
     p_name();
     system("clear||cls");
-    while (l) //loop will continue until l==1
+    while (l) // Loop will continue until l==1
     {
         g_name();
         design();
@@ -76,23 +93,28 @@ void game()
         system("clear||cls");
     }
 }
+
 void g_name()
 {
     printf("\n\n\t\t\t  " B DU BGRN "TicTacToe" RES "\n");
 }
+
 void p_name()
 {
     printf(B RBLU "\tEnter Player1 name: ");
-    while (getchar() != '\n'); //clear input buffer
+    while (getchar() != '\n')
+        ; // Clear input buffer
     scanf("%[^\n]", p1);
-    strcpy(p1_n[pl], p1); //copy player1 name
+    strcpy(p1_n[pl], p1); // Copy player1 name
     printf(RMAG "\tEnter Player2 name: ");
-    while (getchar() != '\n');
+    while (getchar() != '\n')
+        ;
     scanf("%[^\n]", p2);
-    strcpy(p2_n[pl], p2); //copy player2 name
+    strcpy(p2_n[pl], p2); // Copy player2 name
     pl++;
     printf(RES);
 }
+
 void design()
 {
     printf("\n");
@@ -101,13 +123,14 @@ void design()
     puts("\t\t\t|---|---|---|");
     printf("\t\t\t| " YLO "%c" RES B RBBLU RYLO " | " YLO "%c" RES B RBBLU RYLO " | " YLO "%c" RES B RBBLU RYLO " |\n", a[3], a[4], a[5]);
     puts("\t\t\t|---|---|---|");
-    printf("\t\t\t| "YLO "%c" RES B RBBLU RYLO " | " YLO "%c" RES B RBBLU RYLO " | " YLO "%c" RES B RBBLU RYLO " |\n", a[6], a[7], a[8]);
+    printf("\t\t\t| " YLO "%c" RES B RBBLU RYLO " | " YLO "%c" RES B RBBLU RYLO " | " YLO "%c" RES B RBBLU RYLO " |\n", a[6], a[7], a[8]);
     puts("\t\t\t|---|---|---|" RES);
 }
+
 void input_pos()
 {
     char ch;
-    int pos,i, flag1;
+    int pos, i, flag1 = 0;
     printf(B);
     if (i_pos == 0)
     {
@@ -116,9 +139,9 @@ void input_pos()
             if (flag1 == 1)
                 printf(RRED "\n\tWrong Input!");
 
-            printf(RBLU "\n\t%s Enter your position: ", p1); //take position input from player 1  
-            scanf("%d",&pos);
-            ch = 48+(char)pos;
+            printf(RBLU "\n\t%s Enter your position: ", p1); // Take position input from player 1
+            scanf("%d", &pos);
+            ch = 48 + (char)pos;
             if (ch == '0' || ch == 'x')
                 flag1 = 1;
             else
@@ -143,9 +166,9 @@ void input_pos()
         {
             if (flag1 == 1)
                 printf(RRED "\n\tWrong Input!");
-            printf(RMAG "\n\t%s Enter your position: ", p2); //take position input from player 2
-            scanf("%d",&pos);
-            ch = 48+(char)pos;
+            printf(RMAG "\n\t%s Enter your position: ", p2); // Take position input from player 2
+            scanf("%d", &pos);
+            ch = 48 + (char)pos;
             if (ch == '0' || ch == 'x')
                 flag1 = 1;
             else
@@ -167,6 +190,7 @@ void input_pos()
     count++;
     printf(RES);
 }
+
 int chk_pos()
 {
     if ((a[0] == 'x' && a[1] == 'x' && a[2] == 'x') || (a[0] == 'x' && a[3] == 'x' && a[6] == 'x') || (a[0] == 'x' && a[4] == 'x' && a[8] == 'x'))
@@ -192,24 +216,27 @@ int chk_pos()
     else
         return (2);
 }
+
 void result()
 {
     int r = chk_pos();
     if (count < 9)
     {
-        if (r == 2) ;
+        if (r == 2)
+            ; // Continue the game
         else
-            final(r); //pass argument to print result
+            final(r); // Display the game result
     }
     else
         final(r);
 }
+
 void final(int r)
 {
     if (r == 0)
     {
         printf(B RBLU "\n\t%s Win\n" RES, p1);
-        strcpy(win[m], p1); //copy winner name
+        strcpy(win[m], p1); // Copy the winner's name
         p1_win++;
     }
     else if (r == 1)
@@ -230,6 +257,7 @@ void final(int r)
     m++;
     h_scr();
 }
+
 void h_scr()
 {
     l = 0;
@@ -238,6 +266,7 @@ void h_scr()
         ;
     getchar();
 }
+
 void g_result()
 {
     system("clear||cls");
@@ -255,5 +284,4 @@ void g_result()
         printf(RES);
     }
     h_scr();
-return 0;
 }
